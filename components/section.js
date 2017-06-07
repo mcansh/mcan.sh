da05a9cbf4acbd984add72e3f1c341a03bcec363
Logan McAnsh <logan@mcan.sh>
2017-06-07 14:34:23 -0400

    :rabbit:

---
+++ components/section.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components'
+
+const Section = styled.section`
+  background-color: inherit;
+  z-index: 2;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  justify-content: center;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  -webkit-box-orient: vertical;
+  -webkit-box-direction: normal;
+  -ms-flex-direction: column;
+  flex-direction: column;
+  min-height: 100vh;
+  max-width: 600px;
+  width: 80%;
+  margin: 0 auto 50px auto;
+  background: #F3F4F6;
+`
+export default Section
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Page from '../components/page';
+import Heading from '../components/heading';
+import Headshot from '../components/headshot';
+
+export default () => (
+  <Page>
+    <Headshot/>
+    <Heading>Logan McAnsh</Heading>
+    <p>Front End Web Developer for <a target="_blank" href="http://powerley.com">Powerley</a>. Student at <a target="_blank" href="https://flatironschool.com/programs/online-web-developer-career-course/">Learn.co</a> taking the Full-Stack Web Developer course. You can keep up with projects from both on <a target="_blank" href="https://github.com/mcansh">GitHub</a>. I occasionally <a target="_blank" href="https://twitter.com/@logansmcansh">tweet</a>.</p>
+  </Page>
+)
