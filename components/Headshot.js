3b040ffafbae03dbffac65a8328b3c6b0393d257
Logan McAnsh <logan@mcan.sh>
2018-01-18 21:55:07 -0500

    Headshot: conver to class; use object-fit-images as a polyfill
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- components/Headshot.js
+++ components/Headshot.js
@@ -1,19 +1,28 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, Component } from 'react';
+import objectFitImages from 'object-fit-images';
 
-const Headshot = () => (
-  <Fragment>
-    <img src="/static/images/11698668.jpg" alt="me 💁‍♂️" />
-    <style jsx>{`
-      img {
-        height: 15rem;
-        width: 15rem;
-        object-fit: cover;
-        border-radius: 50%;
-        margin: 0 auto;
-        font-family: 'object-fit: cover;';
-      }
-    `}</style>
-  </Fragment>
-);
+class Headshot extends Component {
+  componentDidMount = () => {
+    objectFitImages();
+  };
+
+  render() {
+    return (
+      <Fragment>
+        <img src="/static/images/11698668.jpg" alt="me 💁‍♂️" />
+        <style jsx>{`
+          img {
+            height: 15rem;
+            width: 15rem;
+            object-fit: cover;
+            border-radius: 50%;
+            margin: 0 auto;
+            font-family: 'object-fit: cover;';
+          }
+        `}</style>
+      </Fragment>
+    );
+  }
+}
 
 export default Headshot;
