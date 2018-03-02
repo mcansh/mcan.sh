aa7a339aefc9106fdcb972c1340b39e2f74b3a99
Logan McAnsh <logan@mcan.sh>
2018-03-02 14:54:19 -0500

    convert Index to class, add build time
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Page from '../components/Page';
 import Link from '../components/Link';
 import Headshot from '../components/Headshot';
@@ -6,19 +7,37 @@ import H2 from '../components/Heading';
 import P from '../components/Paragraph';
 import Footer from '../components/Footer';
 
-const Index = () => (
-  <Page>
-    <Headshot />
-    <H2>Logan McAnsh</H2>
-    <P>
-      Making PWAs for <Link href="https://www.powerley.com">Powerley</Link>.
-      Student at{' '}
-      <Link href="https://flatironschool.com/programs/online-web-developer-career-course/">
-        Learn.co
-      </Link>.
-    </P>
-    <Footer />
-  </Page>
-);
+class Index extends Component {
+  static getInitialProps() {
+    const buildTime = Date.now();
+
+    return { buildTime };
+  }
+
+  static propTypes = {
+    buildTime: PropTypes.number.isRequired,
+  };
+
+  componentDidMount = () => {
+    console.log(`built at: ${this.props.buildTime}`);
+  };
+
+  render() {
+    return (
+      <Page>
+        <Headshot />
+        <H2>Logan McAnsh</H2>
+        <P>
+          Making PWAs for <Link href="https://www.powerley.com">Powerley</Link>.
+          Student at{' '}
+          <Link href="https://flatironschool.com/programs/online-web-developer-career-course/">
+            Learn.co
+          </Link>.
+        </P>
+        <Footer />
+      </Page>
+    );
+  }
+}
 
 export default Index;
