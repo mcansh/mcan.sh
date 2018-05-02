0f18aacc3c8e02e38cb4188e52a3ada401d44594
Logan McAnsh <logan@mcan.sh>
2018-05-02 11:04:01 -0400

    remove build time, remove exportPathMap
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@ const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
 const { name } = require('./package.json');
 
 module.exports = {
-  exportPathMap: () => ({
-    '/': { page: '/' }
-  }),
   webpack: config => {
     if (process.env.NODE_ENV === 'production') {
       config.plugins.push(
@@ -14,18 +11,18 @@ module.exports = {
           filename: 'sw.js',
           verbose: true,
           staticFileGlobs: [
-            'static/**/*' // Precache all static files by default
+            'static/**/*', // Precache all static files by default
           ],
           staticFileGlobsIgnorePatterns: [/\.next\//],
           runtimeCaching: [
             {
               handler: 'networkFirst',
-              urlPattern: /^https?.*/
-            }
-          ]
+              urlPattern: /^https?.*/,
+            },
+          ],
         })
       );
     }
     return config;
-  }
+  },
 };
