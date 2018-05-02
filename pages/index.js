a1231d006f541ccf49d24a671da170ae92cbf931
Logan McAnsh <logan@mcan.sh>
2018-05-02 11:04:01 -0400

    remove build time, remove exportPathMap
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
 import Page from '../components/Page';
 import Link from '../components/Link';
 import Headshot from '../components/Headshot';
@@ -7,37 +6,19 @@ import H2 from '../components/Heading';
 import P from '../components/Paragraph';
 import Footer from '../components/Footer';
 
-class Index extends Component {
-  static getInitialProps() {
-    const buildTime = Date.now();
-
-    return { buildTime };
-  }
-
-  static propTypes = {
-    buildTime: PropTypes.number.isRequired,
-  };
-
-  componentDidMount = () => {
-    console.log(`built at: ${this.props.buildTime}`);
-  };
-
-  render() {
-    return (
-      <Page>
-        <Headshot />
-        <H2>Logan McAnsh</H2>
-        <P>
-          Making PWAs for <Link href="https://www.powerley.com">Powerley</Link>.
-          Student at{' '}
-          <Link href="https://flatironschool.com/programs/online-web-developer-career-course/">
-            Learn.co
-          </Link>.
-        </P>
-        <Footer />
-      </Page>
-    );
-  }
-}
+const Index = () => (
+  <Page>
+    <Headshot />
+    <H2>Logan McAnsh</H2>
+    <P>
+      Making PWAs for <Link href="https://www.powerley.com">Powerley</Link>.
+      Student at{' '}
+      <Link href="https://flatironschool.com/programs/online-web-developer-career-course/">
+        Learn.co
+      </Link>.
+    </P>
+    <Footer />
+  </Page>
+);
 
 export default Index;
