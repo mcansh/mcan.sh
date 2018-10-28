cf58ebfcd249e7c26552ee71a3e79aa25a0b7703
Logan McAnsh <logan@mcan.sh>
2018-10-28 15:31:53 -0400

    copy sw during export
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
+const fs = require('fs');
+const { join } = require('path');
+const { promisify } = require('util');
 const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
 const { name } = require('./package.json');
 
+const copyFile = promisify(fs.copyFile);
+
 module.exports = {
+  exportPathMap: async (defaultPathMap, { dev, dir, outDir }) => {
+    if (dev) return defaultPathMap;
+
+    await copyFile(join(dir, '.next', 'sw.js'), join(outDir, 'sw.js'));
+    return defaultPathMap;
+  },
   webpack: config => {
     if (process.env.NODE_ENV === 'production') {
       config.plugins.push(
