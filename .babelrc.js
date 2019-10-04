768eb74ad038cfb03193d62029711098c8565ea2
Logan McAnsh <logan@mcan.sh>
2019-10-03 23:56:54 -0400

    generic rewrite
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- .babelrc.js
+++ .babelrc.js
@@ -1,4 +1,4 @@
 module.exports = {
   presets: ['next/babel'],
-  plugins: ['styled-components', 'inline-dotenv', 'polished'],
+  plugins: ['styled-components', 'root-import', 'polished'],
 };
--- components/Headshot.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Image from './Image';
-
-const Headshot = () => (
-  <Image source="/static/images/11698668.png" alt="me 💁‍♂️" />
-);
-
-export default Headshot;
