b2be0a4935f8ee4f10bd4605ffd48034d6271b7e
Logan McAnsh <logan@mcan.sh>
2020-04-15 18:33:25 -0400

    feat: add fathom analytics
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import Head from 'next/head';
-import App from 'next/app';
+import { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
 import { LinkProvider } from '@mcansh/custom-next-link';
+import Router from 'next/router';
+import * as Fathom from 'fathom-client';
 
 import { GlobalStyle } from '~/components/style/global-style';
 import { version, repository, description } from '~/package.json';
@@ -10,57 +12,65 @@ import { theme } from '~/config';
 
 const iconSizes = [32, 57, 72, 96, 120, 128, 144, 152, 195, 228];
 
-if (typeof window !== 'undefined') {
-  const info = [
-    `Version: ${version}`,
-    `Next.js Build: ${process.env.BUILD_ID}`,
-    `You can find the code here: https://github.com/${repository}`,
-  ];
-  // eslint-disable-next-line no-console
-  info.forEach(message => console.log(message));
-}
+Router.events.on('routeChangeComplete', () => {
+  Fathom.trackPageview();
+});
 
-class MyApp extends App {
-  public render() {
-    const { Component, pageProps } = this.props;
+const App: React.FC<AppProps> = ({ Component, pageProps }) => {
+  React.useEffect(() => {
+    if (process.env.NODE_ENV === 'production') {
+      Fathom.load();
+      Fathom.setSiteId('EPVCGNZL');
+      Fathom.trackPageview();
+    }
+  }, []);
 
-    return (
-      <ThemeProvider theme={theme}>
-        <LinkProvider value="https://mcan.sh">
-          <Head>
-            <title>Logan McAnsh</title>
-            <meta name="description" content={description} />
-            <meta
-              name="viewport"
-              content="initial-scale=1.0, width=device-width, viewport-fit=cover"
-            />
-            <link rel="manifest" href="/manifest.webmanifest" />
-            <link rel="mask-icon" href="/logo.svg" color="#ea567c" />
-            <meta
-              name="apple-mobile-web-app-status-bar-style"
-              content="black-translucent"
-            />
-            <link rel="icon" href="/favicon.png" />
-            <link rel="icon" href="/favicon.ico" />
-            <meta name="theme-color" content="#1d2330" />
-            {iconSizes.map(icon => {
-              const size = `${icon}x${icon}`;
-              return (
-                <link
-                  key={size}
-                  rel="apple-touch-icon"
-                  sizes={size}
-                  href={`/static/images/logo/logo-${icon}.png`}
-                />
-              );
-            })}
-          </Head>
-          <GlobalStyle />
-          <Component {...pageProps} />
-        </LinkProvider>
-      </ThemeProvider>
-    );
-  }
-}
+  React.useEffect(() => {
+    const info = [
+      `Version: ${version}`,
+      `Next.js Build: ${process.env.BUILD_ID}`,
+      `You can find the code here: https://github.com/${repository}`,
+    ];
+    // eslint-disable-next-line no-console
+    info.forEach(message => console.log(message));
+  }, []);
 
-export default MyApp;
+  return (
+    <ThemeProvider theme={theme}>
+      <LinkProvider value="https://mcan.sh">
+        <Head>
+          <title>Logan McAnsh</title>
+          <meta name="description" content={description} />
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width, viewport-fit=cover"
+          />
+          <link rel="manifest" href="/manifest.webmanifest" />
+          <link rel="mask-icon" href="/logo.svg" color="#ea567c" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="black-translucent"
+          />
+          <link rel="icon" href="/favicon.png" />
+          <link rel="icon" href="/favicon.ico" />
+          <meta name="theme-color" content="#1d2330" />
+          {iconSizes.map(icon => {
+            const size = `${icon}x${icon}`;
+            return (
+              <link
+                key={size}
+                rel="apple-touch-icon"
+                sizes={size}
+                href={`/static/images/logo/logo-${icon}.png`}
+              />
+            );
+          })}
+        </Head>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </LinkProvider>
+    </ThemeProvider>
+  );
+};
+
+export default App;
