a5d659817b2fc44f1c6b65991364f95ea4502943
Logan McAnsh <logan@mcan.sh>
2021-05-11 12:53:42 -0400

    feat: toy around with having component css as own file
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- app/components/fun-hover-link.tsx
+++ app/components/fun-hover-link.tsx
@@ -1,14 +1,25 @@
 import * as React from 'react';
 
+import styles from '../styles/fun-hover-link.css';
+
 type Props = React.DetailedHTMLProps<
   React.AnchorHTMLAttributes<HTMLAnchorElement>,
   HTMLAnchorElement
 >;
 
-const FunHoverLink: React.FC<Props> = ({ children, ...rest }) => (
+interface FunctionComponentStyles<P> extends React.FC<P> {
+  styles: string;
+}
+
+const FunHoverLink: FunctionComponentStyles<Props> = ({
+  children,
+  ...rest
+}) => (
   <a {...rest} className="fun-link">
     {children}
   </a>
 );
+
+FunHoverLink.styles = styles;
 
 export { FunHoverLink };
