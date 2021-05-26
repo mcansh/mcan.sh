b92f3083327aa445c5e303f1b560147dab8c50cb
Logan McAnsh <logan@mcan.sh>
2021-05-26 11:54:34 -0400

    chore: remove animation
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -31,7 +31,7 @@ const links: LinksFunction = () => [
 ];
 
 const IndexPage: RouteComponent = () => (
-  <div className="flex flex-col items-center justify-center h-full max-w-screen-md px-4 py-8 mx-auto motion-safe:animate-fall">
+  <div className="flex flex-col items-center justify-center h-full max-w-screen-md px-4 py-8 mx-auto">
     <div className="flex flex-col items-center justify-center flex-1">
       <img
         src="/me.jpg"
