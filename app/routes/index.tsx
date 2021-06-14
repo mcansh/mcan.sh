d88e43c6422e7318148590a8006d93aec8fa14ec
Logan McAnsh <logan@mcan.sh>
2021-06-14 11:52:08 -0400

    feat: safe area
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -31,12 +31,7 @@ const links: LinksFunction = () => [
 ];
 
 const IndexPage: RouteComponent = () => (
-  <div 
-    style={{
-      paddingBottom: "max(2rem, env(safe-area-inset-bottom))"
-    }}
-    className="flex flex-col items-center justify-center h-full max-w-screen-md px-4 py-8 mx-auto"
-  >
+  <div className="flex flex-col items-center justify-center h-screen max-w-screen-md px-4 mx-auto py-8-safe">
     <div className="flex flex-col items-center justify-center flex-1">
       <img
         src="/me.jpg"
--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,7 @@ module.exports = {
   plugins: [
     require('tailwindcss'),
     require('autoprefixer'),
+    require('postcss-100vh-fix'),
     process.env.NODE_ENV === 'production' && require('cssnano'),
   ].filter(Boolean),
 };
