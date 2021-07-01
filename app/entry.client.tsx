2f4201ee1e605d31c6439862e45c50cb5b66bc8a
Logan McAnsh <logan@mcan.sh>
2021-06-30 23:04:37 -0400

    chore: move Fathom setup to entry.client
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,4 +1,11 @@
 import ReactDOM from 'react-dom';
 import { RemixBrowser } from 'remix';
+import * as Fathom from 'fathom-client';
+
+Fathom.load('EPVCGNZL', {
+  excludedDomains: ['localhost'],
+  url: 'https://kiwi.mcan.sh/script.js',
+  spa: 'auto',
+});
 
 ReactDOM.createRoot(document, { hydrate: true }).render(<RemixBrowser />);
--- app/root.tsx
+++ app/root.tsx
@@ -1,12 +1,11 @@
 import * as React from 'react';
 import { Meta, Links, Scripts, LiveReload } from 'remix';
 import type { LinksFunction } from 'remix';
-import { Outlet, useLocation } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import type {
   ErrorBoundaryComponent,
   RouteComponent,
 } from '@remix-run/react/routeModules';
-import * as Fathom from 'fathom-client';
 
 import tailwindUrl from './styles/global.css';
 import interUrl from './styles/inter.css';
@@ -24,73 +23,56 @@ const links: LinksFunction = () => [
   },
 ];
 
-const Document: React.FC = ({ children }) => {
-  const location = useLocation();
+const Document: React.FC = ({ children }) => (
+  <html lang="en" className="h-screen">
+    <head>
+      <meta charSet="utf-8" />
+      <link rel="icon" href="/favicon.png" type="image/png" />
+      <meta
+        name="viewport"
+        content="initial-scale=1.0, width=device-width, viewport-fit=cover"
+      />
+      <link rel="manifest" href="/manifest.webmanifest" />
+      <meta
+        name="apple-mobile-web-app-status-bar-style"
+        content="black-translucent"
+      />
+      <link rel="icon" href="/favicon.png" />
+      <link rel="icon" href="/favicon.ico" />
 
-  React.useEffect(() => {
-    Fathom.load('EPVCGNZL', {
-      excludedDomains: ['localhost'],
-      url: 'https://kiwi.mcan.sh/script.js',
-    });
-  }, []);
-
-  React.useEffect(() => {
-    Fathom.trackPageview({
-      url: location.pathname,
-    });
-  }, [location.pathname]);
-
-  return (
-    <html lang="en" className="h-screen">
-      <head>
-        <meta charSet="utf-8" />
-        <link rel="icon" href="/favicon.png" type="image/png" />
-        <meta
-          name="viewport"
-          content="initial-scale=1.0, width=device-width, viewport-fit=cover"
-        />
-        <link rel="manifest" href="/manifest.webmanifest" />
-        <meta
-          name="apple-mobile-web-app-status-bar-style"
-          content="black-translucent"
-        />
-        <link rel="icon" href="/favicon.png" />
-        <link rel="icon" href="/favicon.ico" />
-
-        <meta
-          name="theme-color"
-          content="#fff"
-          // @ts-expect-error Safari 15 supports this
-          media="(prefers-color-scheme: light)"
-        />
-        <meta
-          name="theme-color"
-          content="#1d2330"
-          // @ts-expect-error Safari 15 supports this
-          media="(prefers-color-scheme: dark)"
-        />
-        {iconSizes.map(icon => {
-          const size = `${icon}x${icon}`;
-          return (
-            <link
-              key={size}
-              rel="apple-touch-icon"
-              sizes={size}
-              href={`/static/images/logo/logo-${icon}.png`}
-            />
-          );
-        })}
-        <Meta />
-        <Links />
-      </head>
-      <body className="h-screen bg-white dark:bg-gray-800 dark:text-white">
-        {children}
-        <Scripts />
-        {process.env.NODE_ENV === 'development' && <LiveReload />}
-      </body>
-    </html>
-  );
-};
+      <meta
+        name="theme-color"
+        content="#fff"
+        // @ts-expect-error Safari 15 supports this
+        media="(prefers-color-scheme: light)"
+      />
+      <meta
+        name="theme-color"
+        content="#1d2330"
+        // @ts-expect-error Safari 15 supports this
+        media="(prefers-color-scheme: dark)"
+      />
+      {iconSizes.map(icon => {
+        const size = `${icon}x${icon}`;
+        return (
+          <link
+            key={size}
+            rel="apple-touch-icon"
+            sizes={size}
+            href={`/static/images/logo/logo-${icon}.png`}
+          />
+        );
+      })}
+      <Meta />
+      <Links />
+    </head>
+    <body className="h-screen bg-white dark:bg-gray-800 dark:text-white">
+      {children}
+      <Scripts />
+      {process.env.NODE_ENV === 'development' && <LiveReload />}
+    </body>
+  </html>
+);
 
 const App: RouteComponent = () => (
   <Document>
