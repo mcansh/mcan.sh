15dba828754ea74f5c00af079ae0a7a1e8c1ceda
Logan McAnsh <logan@mcan.sh>
2021-08-25 22:36:21 -0400

    chore(resume): remove non-functional print button
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -112,9 +112,6 @@ const ResumePage: RouteComponent = () => {
 
   return (
     <div className="relative h-full border-t-8 border-indigo-600 border-solid pb-8-safe">
-      <button className="absolute top-0 right-0" type="button">
-        Print
-      </button>
       <div className="py-4 mx-auto max-w-prose">
         <header className="flex flex-col items-center px-4 pb-2 mb-2 space-x-4 space-y-1 text-center sm:flex-row sm:text-left">
           <div className="relative w-32 h-32 overflow-hidden rounded-full sm:w-40 sm:h-40">
