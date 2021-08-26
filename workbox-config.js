7dd82dcf72da30e6380eb2b02c384a531d92bb06
Logan McAnsh <logan@mcan.sh>
2021-08-25 21:53:13 -0400

    chore: remove workbox
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- workbox-config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  globDirectory: 'public/',
-  globPatterns: ['**/*.{css,js,svg,ico,png,woff,woff2}'],
-  ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
-  swDest: 'public/sw.js',
-};
