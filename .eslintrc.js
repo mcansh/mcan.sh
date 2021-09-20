b6e3b14844844b7590f6f07f97d3068554ff7bd1
Logan McAnsh <logan@mcan.sh>
2021-09-20 09:21:38 -0400

    chore: update tsconfig, move to root
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,5 @@
-const path = require('path');
-
 module.exports = {
   extends: ['@mcansh/eslint-config/typescript'],
-  parserOptions: {
-    project: [path.join(process.cwd(), 'app/tsconfig.json')],
-  },
   rules: {
     'import/no-extraneous-dependencies': [
       'error',
