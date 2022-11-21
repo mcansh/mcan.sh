490c3ccf2e7e2e7fa4c94e277d930bd2ac447ce1
Logan McAnsh <logan@mcan.sh>
2022-11-21 17:20:31 -0500

    chore(FunHoverLink): detect external url and render good ol anchor tag
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- app/components/fun-hover-link.tsx
+++ app/components/fun-hover-link.tsx
@@ -1,18 +1,31 @@
-import * as React from "react";
+import type { LinkProps } from "@remix-run/react";
+import { Link } from "@remix-run/react";
 
-type Props = React.DetailedHTMLProps<
-  React.AnchorHTMLAttributes<HTMLAnchorElement>,
-  HTMLAnchorElement
->;
+function FunHoverLink({ children, to, ...rest }: LinkProps) {
+  let href = to.toString();
+  let isExternal = /^https?:\/\//.test(href);
 
-const FunHoverLink: React.FC<Props> = ({ children, ...rest }) => (
-  <a
-    {...rest}
-    className="fun-link relative bg-no-repeat text-current no-underline"
-  >
-    {children}
-  </a>
-);
+  if (isExternal) {
+    return (
+      <a
+        {...rest}
+        className="fun-link relative bg-no-repeat text-current no-underline"
+        href={href}
+      >
+        {children}
+      </a>
+    );
+  }
+  return (
+    <Link
+      {...rest}
+      className="fun-link relative bg-no-repeat text-current no-underline"
+      to={href}
+    >
+      {children}
+    </Link>
+  );
+}
 
 export { default as styles } from "~/styles/fun-hover-link.css";
 export { FunHoverLink };
