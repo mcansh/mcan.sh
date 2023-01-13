3aaaadc4f1ea7fa5e62545c737c9964c4cb39869
Logan McAnsh <logan@mcan.sh>
2023-01-13 11:27:36 -0500

    chore: prefetch external links
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- app/components/fun-hover-link.tsx
+++ app/components/fun-hover-link.tsx
@@ -1,24 +1,39 @@
+import * as React from "react";
 import type { LinkProps } from "@remix-run/react";
 import { Link } from "@remix-run/react";
 
-function FunHoverLink({ children, to, ...rest }: LinkProps) {
+export { default as styles } from "~/styles/fun-hover-link.css";
+
+export function FunHoverLink({
+  children,
+  to,
+  prefetch = "none",
+  ...props
+}: LinkProps) {
   let href = to.toString();
-  let isExternal = /^https?:\/\//.test(href);
+  let isExternal = /^[a-z]+:/.test(href);
+  let [shouldPrefetch, prefetchHandlers] = usePrefetchBehavior(prefetch, props);
 
   if (isExternal) {
     return (
-      <a
-        {...rest}
-        className="fun-link relative bg-no-repeat text-current no-underline"
-        href={href}
-      >
-        {children}
-      </a>
+      <>
+        <a
+          {...props}
+          className="fun-link relative bg-no-repeat text-current no-underline"
+          href={href}
+          {...prefetchHandlers}
+        >
+          {children}
+        </a>
+        {shouldPrefetch ? (
+          <link rel="prefetch" as="document" href={href} />
+        ) : null}
+      </>
     );
   }
   return (
     <Link
-      {...rest}
+      {...props}
       className="fun-link relative bg-no-repeat text-current no-underline"
       to={href}
     >
@@ -27,5 +42,75 @@ function FunHoverLink({ children, to, ...rest }: LinkProps) {
   );
 }
 
-export { default as styles } from "~/styles/fun-hover-link.css";
-export { FunHoverLink };
+interface PrefetchHandlers {
+  onFocus?: React.FocusEventHandler<Element>;
+  onBlur?: React.FocusEventHandler<Element>;
+  onMouseEnter?: React.MouseEventHandler<Element>;
+  onMouseLeave?: React.MouseEventHandler<Element>;
+  onTouchStart?: React.TouchEventHandler<Element>;
+}
+
+type PrefetchBehavior = "intent" | "render" | "none";
+
+function usePrefetchBehavior(
+  prefetch: PrefetchBehavior,
+  theirElementProps: PrefetchHandlers
+): [boolean, Required<PrefetchHandlers>] {
+  let [maybePrefetch, setMaybePrefetch] = React.useState(false);
+  let [shouldPrefetch, setShouldPrefetch] = React.useState(false);
+  let { onFocus, onBlur, onMouseEnter, onMouseLeave, onTouchStart } =
+    theirElementProps;
+
+  React.useEffect(() => {
+    if (prefetch === "render") {
+      setShouldPrefetch(true);
+    }
+  }, [prefetch]);
+
+  let setIntent = () => {
+    if (prefetch === "intent") {
+      setMaybePrefetch(true);
+    }
+  };
+
+  let cancelIntent = () => {
+    if (prefetch === "intent") {
+      setMaybePrefetch(false);
+      setShouldPrefetch(false);
+    }
+  };
+
+  React.useEffect(() => {
+    if (maybePrefetch) {
+      let id = setTimeout(() => {
+        setShouldPrefetch(true);
+      }, 100);
+      return () => {
+        clearTimeout(id);
+      };
+    }
+  }, [maybePrefetch]);
+
+  return [
+    shouldPrefetch,
+    {
+      onFocus: composeEventHandlers(onFocus, setIntent),
+      onBlur: composeEventHandlers(onBlur, cancelIntent),
+      onMouseEnter: composeEventHandlers(onMouseEnter, setIntent),
+      onMouseLeave: composeEventHandlers(onMouseLeave, cancelIntent),
+      onTouchStart: composeEventHandlers(onTouchStart, setIntent),
+    },
+  ];
+}
+
+function composeEventHandlers<EventType extends React.SyntheticEvent | Event>(
+  theirHandler: ((event: EventType) => any) | undefined,
+  ourHandler: (event: EventType) => any
+): (event: EventType) => any {
+  return (event) => {
+    theirHandler && theirHandler(event);
+    if (!event.defaultPrevented) {
+      ourHandler(event);
+    }
+  };
+}
