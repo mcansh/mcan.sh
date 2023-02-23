9ede23423e9bbe66837e8b7100d1985365725545
Logan McAnsh <logan@mcan.sh>
2023-02-23 10:27:14 -0500

    chore: use double quotes in pnpm-lock
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,12 @@
 /** @type {import('prettier').Config} */
 module.exports = {
   plugins: [require("prettier-plugin-tailwindcss")],
+  overrides: [
+    {
+      files: "./pnpm-lock.yaml",
+      options: {
+        singleQuote: false,
+      },
+    },
+  ],
 };
