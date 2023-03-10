597dd61cee6c269eb37623b9dee5d5ee3bb0d500
Logan McAnsh <logan@mcan.sh>
2023-03-09 20:46:10 -0500

    chore(deps): add @netlify/edge-functions
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- prettier.config.js
+++ prettier.config.js
@@ -1,12 +1,4 @@
 /** @type {import('prettier').Config} */
 module.exports = {
   plugins: [require("prettier-plugin-tailwindcss")],
-  overrides: [
-    {
-      files: "./pnpm-lock.yaml",
-      options: {
-        singleQuote: false,
-      },
-    },
-  ],
 };
