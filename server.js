49e80a6e91f93aca4f88976f574ec73b6c632516
Logan McAnsh <logan@mcan.sh>
2023-03-15 21:51:12 -0400

    feat: railway (#286)
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
-import { createRequestHandler } from "@netlify/remix-edge-adapter";
-import * as build from "@remix-run/dev/server-build";
+let path = require("node:path");
+let fastify = require("fastify");
+let { remixFastifyPlugin } = require("@mcansh/remix-fastify");
 
-export default createRequestHandler({
-  build,
-  mode: process.env.NODE_ENV,
+let MODE = process.env.NODE_ENV;
+
+async function start() {
+  let app = fastify();
+
+  await app.register(remixFastifyPlugin, {
+    build: path.join(process.cwd(), "build/index.js"),
+    mode: MODE,
+    purgeRequireCacheInDevelopment: false,
+  });
+
+  let port = Number(process.env.PORT) || 3000;
+
+  let address = await app.listen({ port, host: "0.0.0.0" });
+  console.log(`✅ app ready: ${address}`);
+}
+
+start().catch((error) => {
+  console.error(error);
+  process.exit(1);
 });
-
-export const config = {
-  cache: "manual",
-  path: "/*",
-};
