48de25effb88c94b8c5ae30cfdaca8cf14f2de9b
Logan McAnsh <logan@mcan.sh>
2023-05-29 16:18:00 -0400

    chore: add domain in production
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- sst.config.ts
+++ sst.config.ts
@@ -1,5 +1,14 @@
 import type { SSTConfig } from "sst";
 import { RemixSite } from "sst/constructs";
+import { Certificate } from "aws-cdk-lib/aws-certificatemanager";
+import { z } from "zod";
+
+let config = z.object({
+  AWS_DOMAIN: z.string(),
+  AWS_CERTIFICATE_ARN: z.string(),
+});
+
+let result = config.parse(process.env);
 
 export default {
   config(_input) {
@@ -12,8 +21,23 @@ export default {
     app.stack(function Site({ stack }) {
       let site = new RemixSite(stack, "site", {
         runtime: "nodejs18.x",
+        customDomain:
+          stack.stage === "prod"
+            ? {
+                isExternalDomain: true,
+                domainName: `www.${result.AWS_DOMAIN}`,
+                alternateNames: [result.AWS_DOMAIN],
+                cdk: {
+                  certificate: Certificate.fromCertificateArn(
+                    stack,
+                    "MyCert",
+                    result.AWS_CERTIFICATE_ARN
+                  ),
+                },
+              }
+            : undefined,
       });
-      stack.addOutputs({ url: site.url });
+      stack.addOutputs({ url: site.customDomainUrl || site.url });
     });
   },
 } satisfies SSTConfig;
