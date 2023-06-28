4b6011244c4b6dfc9be59beebfeb01628af3c0ae
Logan McAnsh <logan@mcan.sh>
2023-06-28 11:06:41 -0400

    chore(deps): bump to latest
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- remix.config.js
+++ remix.config.js
@@ -12,10 +12,11 @@ module.exports = {
   serverBuildPath: "build/index.js",
   serverModuleFormat: "cjs",
   future: {
-    unstable_dev: true,
+    v2_dev: true,
     v2_errorBoundary: true,
     v2_meta: true,
     v2_normalizeFormMethod: true,
     v2_routeConvention: true,
+    v2_headers: true,
   },
 };
