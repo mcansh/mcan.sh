d5d889670aac46e1728afd5d32a69523bc305072
Logan McAnsh <logan@mcan.sh>
2023-07-18 22:46:28 -0400

    feat: add .well-known/avatar route
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

---
+++ app/routes/[.well-known].avatar.ts
@@ -0,0 +1,19 @@
+import type { DataFunctionArgs } from "@remix-run/node";
+
+import { getCloudinaryURL } from "~/cloudinary.server";
+
+export function loader({ request }: DataFunctionArgs) {
+	let url = new URL(request.url);
+
+	let width = url.searchParams.get("w") || url.searchParams.get("width");
+	let height = url.searchParams.get("h") || url.searchParams.get("height");
+
+	if (width && !height) height = width;
+	if (height && !width) width = height;
+
+	let image = getCloudinaryURL("website/11698668", {
+		resize: width && height ? { width, height } : undefined,
+	});
+
+	return fetch(image);
+}
