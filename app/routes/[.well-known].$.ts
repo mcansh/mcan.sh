a2c89542b7b91deef7f1131a76f0a4d117e3a17e
Logan McAnsh <logan@mcan.sh>
2023-09-12 21:30:18 -0400

    chore: unnecessary update to .well-known/avatar that will "validate" its for avatar and forward image transforms to cloudinary
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

---
+++ app/routes/[.well-known].$.ts
@@ -0,0 +1,58 @@
+import type { DataFunctionArgs } from "@remix-run/server-runtime";
+
+import { getCloudinaryURL, MUGSHOT } from "~/cloudinary.server";
+
+let badRequest = new Response("ope", {
+	status: 404,
+	statusText: "Not Found",
+	headers: { "Content-Type": "text/plain" },
+});
+
+export async function loader({ params, request }: DataFunctionArgs) {
+	let splat = params["*"];
+
+	if (!splat) {
+		return badRequest;
+	}
+
+	let segments = splat.split("/");
+	if (segments.at(-1) !== "avatar") {
+		return badRequest;
+	}
+
+	// remove the last segment (avatar)
+	segments = segments.slice(0, -1);
+
+	let image = getCloudinaryURL(MUGSHOT);
+
+	let url = new URL(image);
+
+	let pathSegments = url.pathname.split("/").filter(Boolean);
+
+	let urlSegmentIndex = pathSegments.findIndex((segment) => {
+		return segment.includes(",");
+	});
+
+	if (urlSegmentIndex === -1) {
+		return badRequest;
+	}
+
+	let transformSegment = pathSegments[urlSegmentIndex];
+
+	if (!transformSegment) {
+		return badRequest;
+	}
+
+	// merge our segments with the url segments with a comma
+	transformSegment = transformSegment + "," + segments.join(",");
+
+	// replace the old transform segment with our new one
+	pathSegments[urlSegmentIndex] = transformSegment;
+
+	// replace the pathname with our new segments
+	url.pathname = pathSegments.join("/");
+
+	image = url.toString();
+
+	return fetch(image);
+}
