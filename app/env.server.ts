fae8531ef0dd8176c04e13fdf3f92ab70a9f13c0
Logan McAnsh <logan@mcan.sh>
2023-11-18 18:33:47 -0500

    chore: `@remix-run/dev` properly adds env without `VITE_` prefix to server
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- app/env.server.ts
+++ app/env.server.ts
@@ -1,21 +1,8 @@
 import { z } from "zod";
 
 let envSchema = z.object({
-	VITE_CLOUDINARY_CLOUD_NAME: z.string().optional(),
-	VITE_SENTRY_REPORT_URL: z.string().optional(),
+	CLOUDINARY_CLOUD_NAME: z.string(),
+	SENTRY_REPORT_URL: z.string(),
 });
 
-let _env = envSchema.parse(import.meta.env);
-
-// create type for env variables without VITE_ prefix
-export type EnvKeys = keyof typeof _env;
-
-type Env<T> = T extends `VITE_${infer P}` ? P : never;
-
-// remove VITE_ prefix
-export let env = Object.fromEntries(
-	Object.entries(_env).map(([key, value]) => [
-		key.replace(/^VITE_/, ""),
-		value,
-	]),
-) as unknown as Record<Env<EnvKeys>, string>;
+export let env = envSchema.parse(process.env);
