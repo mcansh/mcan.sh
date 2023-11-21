a281e7a53980993351d2fe31dca468006dfc0065
Logan McAnsh <logan@mcan.sh>
2023-11-21 17:02:24 -0500

    chore: bump bun to latest, fix types that were fixed as a result
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,5 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-	plugins: [
-		remix({ future: { v3_fetcherPersist: true } }),
-		// @ts-expect-error - type issue
-		tsconfigPaths(),
-	],
+	plugins: [remix({ future: { v3_fetcherPersist: true } }), tsconfigPaths()],
 });
