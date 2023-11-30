e128d00675add2501bc35995f4714362509f77f0
Logan McAnsh <logan@mcan.sh>
2023-11-30 15:55:46 -0500

    chore: install deps with pnpm
    using bun causes fouc for some reason...
    
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- scripts/prepare.js
+++ scripts/prepare.js
@@ -5,8 +5,7 @@ let isCi = process.env.CI !== undefined;
 if (!isCi) {
 	let husky = await import("husky");
 	husky.install();
-	console.log(`installed husky hooks`);
 }
 
 console.log(`patching packages`);
-execSync("bunx patch-package", { stdio: "inherit" });
+execSync("pnpm dlx patch-package", { stdio: "inherit" });
