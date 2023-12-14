b78e2718b098db83ff19543f5d2c8e18a37209c8
Logan McAnsh <logan@mcan.sh>
2023-12-14 12:41:19 -0500

    chore: split vendor chunks
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,13 @@
 import { unstable_vitePlugin as remix } from "@remix-run/dev";
-import { defineConfig } from "vite";
+import { defineConfig, splitVendorChunkPlugin } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
 	server: { middlewareMode: true },
 	plugins: [
-		remix({
-			future: {
-				v3_fetcherPersist: true,
-				v3_relativeSplatPath: true,
-			},
-		}),
+		remix({ future: { v3_fetcherPersist: true, v3_relativeSplatPath: true } }),
 		tsconfigPaths(),
+		splitVendorChunkPlugin(),
 	],
 	build: {
 		assetsInlineLimit: 0, // keep SVG as asset URL
