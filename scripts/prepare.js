3cc7aec654dcc406a83023f57e7a20fec568d2e0
Logan McAnsh <logan@mcan.sh>
2024-02-28 14:16:00 -0500

    chore(deps): bump to latest
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- scripts/prepare.js
+++ scripts/prepare.js
@@ -3,8 +3,8 @@ import { execSync } from "node:child_process";
 let isCi = process.env.CI !== undefined;
 
 if (!isCi) {
-	let husky = await import("husky");
-	husky.install();
+	let husky = await import("husky").then((m) => m.default);
+	husky();
 	// format pnpm-lock.yaml
 	execSync(`pnpm run format ./pnpm-lock.yaml`, { stdio: "inherit" });
 }
