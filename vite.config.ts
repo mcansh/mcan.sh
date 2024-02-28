d8f4cd9f68e183e84634379a74bd1f126c6a1f6f
Logan McAnsh <logan@mcan.sh>
2024-02-28 14:47:21 -0500

    chore: remove million
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import { vitePlugin as remix } from "@remix-run/dev";
-import { vite as million } from "million/compiler";
 import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig, splitVendorChunkPlugin } from "vite";
 import type { Plugin } from "vite";
@@ -12,7 +11,6 @@ export default defineConfig({
 	plugins: [
 		tsconfigPaths(),
 		splitVendorChunkPlugin(),
-		million({ auto: true, server: true }),
 		EMIT_FILE ? visualizer({ emitFile: true }) : null,
 		remix({
 			future: {
