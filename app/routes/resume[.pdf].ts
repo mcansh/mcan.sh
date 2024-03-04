bfca41dddcbf25062a644191f201181ae6d570be
Logan McAnsh <logan@mcan.sh>
2024-03-04 17:17:17 -0500

    chore: update filename
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- app/routes/resume[.pdf].ts
+++ app/routes/resume[.pdf].ts
@@ -4,13 +4,11 @@ import { cacheHeader } from "pretty-cache-header";
 
 export async function loader({ request }: LoaderFunctionArgs) {
 	let url = new URL(request.url);
-
 	let resume_url = new URL("resume", url.origin);
 
 	let browser = await chromium.launch();
 	let page = await browser.newPage();
 	await page.goto(resume_url.toString());
-
 	let pdf = await page.pdf({
 		tagged: true,
 		margin: {
@@ -26,7 +24,7 @@ export async function loader({ request }: LoaderFunctionArgs) {
 	return new Response(pdf, {
 		headers: {
 			"Content-Type": "application/pdf",
-			"Content-Disposition": "inline; filename=resume.pdf",
+			"Content-Disposition": "inline; filename=Logan McAnsh.pdf",
 			"Cache-Control": cacheHeader({
 				private: true,
 			}),
