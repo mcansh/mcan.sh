5140619960642a4aece0e6a96d176712e3041054
Logan McAnsh <logan@mcan.sh>
2024-03-03 20:17:41 -0500

    feat: generate resume.pdf on demand
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

---
+++ app/routes/resume[.pdf].ts
@@ -0,0 +1,27 @@
+import type { LoaderFunctionArgs } from "@remix-run/node";
+import { chromium } from "playwright";
+import { cacheHeader } from "pretty-cache-header";
+
+export async function loader({ request }: LoaderFunctionArgs) {
+	let url = new URL(request.url);
+
+	let resume_url = new URL("resume", url.origin);
+
+	let browser = await chromium.launch();
+	let page = await browser.newPage();
+	await page.goto(resume_url.toString());
+
+	let pdf = await page.pdf();
+	await browser.close();
+
+	return new Response(pdf, {
+		headers: {
+			"Content-Type": "application/pdf",
+			"Content-Disposition": "inline; filename=resume.pdf",
+			"Cache-Control": cacheHeader({
+				sMaxage: "1h",
+				staleWhileRevalidate: "1w",
+			}),
+		},
+	});
+}
