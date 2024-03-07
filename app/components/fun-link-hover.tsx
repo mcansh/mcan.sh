cca8bcb55c97da95cc32bcfdec3891c45565ef13
Logan McAnsh <logan@mcan.sh>
2024-03-07 16:24:19 -0500

    chore(deps): bump react canary to latest
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- app/components/fun-link-hover.tsx
+++ app/components/fun-link-hover.tsx
@@ -5,8 +5,8 @@ export function FunHoverLink({ children, to, ...props }: LinkProps) {
 	return (
 		<Link
 			className="from-link to-link relative cursor-pointer bg-gradient-to-t bg-[100%_100%] bg-[length:100%_2px] bg-no-repeat text-center font-thin leading-6 no-underline transition-all duration-200 hover:bg-[length:100%_100%] hover:text-white"
-			{...props}
 			to={to}
+			{...props}
 		>
 			{children}
 		</Link>
