5cebcd267b9379f115623421ec48f43497dffb78
Logan McAnsh <logan@mcan.sh>
2024-03-08 16:53:58 -0500

    fix: move bg-size to app.css
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- app/components/fun-link-hover.tsx
+++ app/components/fun-link-hover.tsx
@@ -4,7 +4,7 @@ import { Link } from "@remix-run/react";
 export function FunHoverLink({ children, to, ...props }: LinkProps) {
 	return (
 		<Link
-			className="from-link to-link relative cursor-pointer bg-gradient-to-t bg-[100%_100%] bg-[length:100%_2px] bg-no-repeat text-center font-thin leading-6 no-underline transition-all duration-200 hover:bg-[length:100%_100%] hover:text-white"
+			className="from-link to-link fun-link-hover-gradient relative cursor-pointer bg-gradient-to-t bg-[100%_100%] bg-no-repeat text-center font-thin leading-6 no-underline transition-all duration-200 hover:text-white"
 			to={to}
 			{...props}
 		>
