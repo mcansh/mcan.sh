7c20a184692aea1954968f32dcc5e501b8f38b22
Logan McAnsh <logan@mcan.sh>
2024-09-20 09:49:03 -0400

    Update routes.ts

--- app/routes.ts
+++ app/routes.ts
@@ -3,7 +3,7 @@ import type { RouteConfig } from "@react-router/dev/routes";
 
 export const routes = [
 	index("./routes/_index.tsx"),
-	route("/.well-known/*", "./routes/[.well-known]/$.ts"),
+	route("/.well-known/*", "./routes/[.well-known].$.ts"),
 	route("/resume", "./routes/resume.tsx"),
 	route("/resume.pdf", "./routes/resume[.pdf].ts"),
 ] satisfies RouteConfig;
