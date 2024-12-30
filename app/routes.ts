05e6d9ad54878a163170d45bb83e264529567024
Logan McAnsh <logan@mcan.sh>
2024-12-30 18:02:13 -0500

    chore: remove resume pdf route
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- app/routes.ts
+++ app/routes.ts
@@ -5,6 +5,5 @@ export default [
 	index("./routes/home.tsx"),
 	route("/.well-known/*", "./routes/well-known.$.ts"),
 	route("/resume", "./routes/resume.tsx"),
-	route("/resume.pdf", "./routes/resume.pdf.ts"),
 	route("/manifest.webmanifest", "./routes/manifest.webmanifest/index.ts"),
 ] satisfies RouteConfig;
