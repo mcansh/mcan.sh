cbf568c6986867727b5312f966e7e42adb8e2c43
Logan McAnsh <logan@mcan.sh>
2025-06-14 12:11:11 -0400

    chore: remove console.log statement from env.ts

--- app/lib/lib.client/env.ts
+++ app/lib/lib.client/env.ts
@@ -9,5 +9,3 @@ export let client_env = client_env_schema.parse({
 	VITE_FATHOM_SITE_ID: import.meta.env.VITE_FATHOM_SITE_ID,
 	VITE_SENTRY_DSN: import.meta.env.VITE_SENTRY_DSN,
 });
-
-console.log({ client_env });
