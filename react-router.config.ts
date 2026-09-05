0504135a2f9e54e99de2ab12bad1d63b90bb488c
Logan McAnsh <logan@mcan.sh>
2026-09-05 12:21:29 -0400

    chore(deps): bump to latest
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,11 +1,9 @@
 import type { Config } from "@react-router/dev/config";
 
 export default {
-	ssr: true,
+  ssr: true,
+  splitRouteModules: "enforce",
 	future: {
-		unstable_optimizeDeps: true,
-		v8_middleware: true,
-		v8_splitRouteModules: "enforce",
-		v8_viteEnvironmentApi: true,
-	},
+    unstable_optimizeDeps: true,
+  },
 } as Config;
